0fa922519b42e000b2a4d754ced8a2ee6da15734
Pranav Bakale <78615142+pranavbakale@users.noreply.github.com>
2021-08-30 15:03:13 +0530

    Update GroupA_AssignmentNo1.py

--- GroupA_AssignmentNo1.py
+++ GroupA_AssignmentNo1.py
@@ -9,31 +9,64 @@ Assignment Number 1 : Write a python program to store marks stored in subject "F
 
 
 
-n = int(input("Enter number of students :"))
-marks_list = []
+n = int(input("Enter number of students :"))                  
+marks_list = []                                         
+absent = []
 print("\nNote : Please enter -1 if Student is Absent :\n")
 count = 0
+
 for i in range(n):
     m = int(input("Enter marks :"))
     if m == -1:
         count += 1
+        student = input("Enter the Name of Absent Students :")
+        absent.append(student)
         continue
     else:
         marks_list.append(m)
 
-avg = sum(marks_list) / len(marks_list)
-marks_list.sort()
+def average():
+    sumOfMarks = 0
+    num = 0
+    for j in range(len(marks_list)):
+        if marks_list[j] != -999:
+            sumOfMarks += marks_list[j]
+            num += 1
+    avg = sumOfMarks / num
+    return avg
+    
+def maximum():
+    for k in range(len(marks_list)):
+        if marks_list[k] != -999:
+            max_marks = marks_list[0]
+            break
+    for k in range(1, len(marks_list)):
+        if marks_list[k] > max_marks:
+            max_marks = marks_list[k]
+    return max_marks
+
+def minimum():
+    for l in range(len(marks_list)):
+        if marks_list[l] != -999:
+            min_marks = marks_list[0]
+            break
+    for l in range(1, len(marks_list)):
+        if marks_list[l] < min_marks:
+            min_marks = marks_list[l]
+    return min_marks
+    
 
 x = 0
 res = marks_list[0]
-for k in marks_list:
-    freq = marks_list.count(k)
+for n in marks_list:
+    freq = marks_list.count(n)
     if freq > x:
         x = freq
-        res = k
+        res = n
 
-print("\nAverage Score of Class is :", avg)
-print("\nHighest Score of Class is :", marks_list[-1])
-print("\nLowest Score of Class is :", marks_list[0])
+print("\nAverage Score of Class is :", average())
+print("\nHighest Score of Class is :", maximum())
+print("\nLowest Score of Class is :", minimum())
 print("\nNo of Absent Students :", count)
+print("\nName of Absent Students for Exam :", absent)
 print("\nMost Frequent Marks is :", res)
